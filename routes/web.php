bf60931f37d8158b3dc1147e49d15d7d3cb4e99e
muhammadwaleed112 <muhammadwaleedsheikh640@gmail.com>
2026-08-13 22:53:17 +0500

    fiveth commit

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/terms-of-use', 'HomeController@terms_of_use')->name('terms_of_use')
 Route::group(['middleware' => 'auth'], function () {
 
     Route::get('/', 'HomeController@dashboard')->name('home');
-    Route::get('/home', 'HomeController@dashboard')->name('home');
+    Route::get('/home', 'HomeController@dashboard');
     Route::get('/dashboard', 'HomeController@dashboard')->name('dashboard');
     Route::get('/dashboard/switch-view/{role}', 'HomeController@switch_dashboard_view')->name('dashboard.switch_view');
 
